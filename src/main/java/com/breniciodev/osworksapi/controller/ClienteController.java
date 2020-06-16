a08b2be71ae04a87c65f2896d99ed31b87f249a7
brenicio <brenicio_guilherme2@hotmail.com>
2020-06-16 13:58:01 -0300

    Adicionando o cliente controller e cliente

--- src/main/java/com/breniciodev/osworksapi/controller/ClienteController.java
+++ src/main/java/com/breniciodev/osworksapi/controller/ClienteController.java
@@ -1,14 +1,34 @@
 package com.breniciodev.osworksapi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.breniciodev.osworksapi.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
 	
 	@GetMapping("/clientes")
-	public String Listar() {
-		return "teste";
+	public List<Cliente> Listar() {
+		Cliente cliente1 = new Cliente();
+		
+		cliente1.setId(1L);
+		cliente1.setNome("João Oliveira");
+		cliente1.setEmail("joao.oliveira@hotmail.com");
+		cliente1.setTelefone("62 98455-6358");
+		
+		Cliente cliente2 = new Cliente();
+		
+		cliente2.setId(2L);
+		cliente2.setNome("Elisama");
+		cliente2.setEmail("elisama.oliveira@hotmail.com");
+		cliente2.setTelefone("62 98425-3568");
+		
+		
+		return Arrays.asList(cliente1, cliente2);
 	}
 
 }
